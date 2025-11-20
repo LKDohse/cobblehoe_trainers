75816edfbec09153ae7affa0cbe215564deb5af2
ElectricBudgie <lkdohse@gmail.com>
2025-11-20 11:33:04 -0500

    initial commit

---
+++ src/main/java/net/electricbudgie/CobblehoeTrainersClient.java
@@ -0,0 +1,10 @@
+package net.electricbudgie;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CobblehoeTrainersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
